f4ef9533a18a986d27a6de34087f7ee87e52a2e7
Tony Kun <42751636+TonyKun7@users.noreply.github.com>
2022-10-29 03:07:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 
+const {
+    Client,
+    Collection,
+    Intents
+} = require('discord.js')
+
+const {token} = require('./config.json')
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Intents.FLAGS.GUILD_INTEGRATIONS,
+        Intents.FLAGS.GUILD_WEBHOOKS,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING
+    ]
+})
+
+
+require('./assets/handler')(client)
+
+client.slashCommands = new Collection()
+client.slashSeconds = new Collection()
+
+client.login(token)
+
+module.exports.client = client
